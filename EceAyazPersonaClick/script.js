f89b3aba0ef5741530e086ae9e03bfd4e15385b1
ayaz <ece.ayazz81@gmail.com>
2024-08-19 11:12:25 +0300

    swiper and popup

---
+++ EceAyazPersonaClick/script.js
@@ -0,0 +1,18 @@
+const openPopupBtn = document.getElementById('openPopupBtn');
+const popup = document.getElementById('popup');
+const closePopupBtn = document.getElementById('closePopupBtn');
+
+openPopupBtn.addEventListener('click', function() {
+    popup.style.display = 'flex'; 
+});
+
+closePopupBtn.addEventListener('click', function() {
+    popup.style.display = 'none'; 
+});
+
+window.addEventListener('click', function(event) {
+    if (event.target === popup) {
+        popup.style.display = 'none'; 
+    }
+});
+
